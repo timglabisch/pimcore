b736250b227c8bfd30d4ad7fd020d3c51b34e69c
Tim Glabisch <tim@ainfach.de>
2012-01-08 19:19:43 -0800

    delete old events

--- pimcore/static/js/pimcore/event.js
+++ pimcore/static/js/pimcore/event.js
@@ -31,7 +31,7 @@ pimcore.event.pimcore = {
         }
     },
     'document': {
-        document: {
+        'document': {
             open : 'pimcore.document.document.open'
         },
         'tree': {
@@ -78,43 +78,3 @@ pimcore.event.pimcore = {
         }
     }
 }
-
-// All Pimcore Core Events
-
-/**
- * @param Ext.Toolbar
- * @see layout/toolbar
- */
-pimcore.event.pimcoreLayoutToolbarRender = 'pimcoreLayoutToolbarRender';
-
-/**
- * @param Ext.tree.TreePanel
- * @see document/tree
- */
-pimcore.event.pimcoreDocumentTreeRender = 'pimcoreDocumentTreeRender';
-
-/**
- * @param Ext.menu.Menu
- * @param int DocumentId
- * @see document/tree
- */
-pimcore.event.pimcoreDocumentTreeContextMenuRender = 'pimcoreDocumentTreeContextMenuRender';
-
-
-/**
- * @param Ext.menu.Menu
- * @param int DocumentId
- * @see asset/tree
- */
-pimcore.event.pimcoreAssetTreeContextMenuRender = 'pimcoreAssetTreeContextMenuRender'
-
-
-/**
- * @param Ext.menu.Menu
- * @param int DocumentId
- * @see object/tree
- */
-pimcore.event.pimcoreObjectTreeContextMenuRender = 'pimcoreObjectTreeContextMenuRender';
-
-
-pimcore.event.pimcoreDocumentPreOpen = "pimcoreDocumentPreOpen";
